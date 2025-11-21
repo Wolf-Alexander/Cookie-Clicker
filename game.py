d7521bfb60da492908bdc82bc26215d8a4b08d24
ResterW <107149894+Wolf-Alexander@users.noreply.github.com>
2025-11-22 00:25:41 +0100

    Implement shop_button class for purchase options
    Added a shop_button class to create multiple buttons for purchasing items in the game.

--- game.py
+++ game.py
@@ -6,7 +6,14 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Simple Game Loop")
 
-button = Button(screen, 100, 100, 300, 150)
+class shop_button:
+    def __init__(self, screen, x, y, width, height, text=" "):
+        self.button = Button(screen, x, y, width, height, text=text)
+
+button1 = shop_button(screen, 650, 70, 300, 30,"Buy Now")
+button2 = shop_button(screen, 650, 110, 300, 30,"Buy Now")
+button3 = shop_button(screen, 650, 150, 300, 30,"Buy Now")
+button4 = shop_button(screen, 650, 190, 300, 30,"Buy Now")
 
 clock = pygame.time.Clock()
 running = True
@@ -23,6 +30,11 @@ while running:
     pygame.draw.line(screen, (255, 255, 255), (boundary_x, 0), (boundary_x, 600), 2)
 
     pygame_widgets.update(events)
+
+    font = pygame.font.Font(None, 36)   # Schriftart & Größe
+    text_surface = font.render("Store", True, (255, 255, 255))  # Text, Anti-Alias, Farbe
+    screen.blit(text_surface, (750, 10))   # Text an (x, y) zeichnen
+
 
     pygame.display.flip()
     clock.tick(60)
